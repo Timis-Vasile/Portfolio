0b322408bcc7796a11042544a18843b53d748d5a
Timis-Vasile <vasile.timis@istitutoagnelli.com>
2025-06-17 14:01:01 +0200

    fixed

---
+++ Portfolio/docs/_app/immutable/nodes/3.CqGyAh7y.js
@@ -0,0 +1,1 @@
+import{f as r,a as p}from"../chunks/BoVA4qQb.js";import"../chunks/CK1MN0Xm.js";var t=r("<h1>ABOUT PAGE</h1>");function e(o){var a=t();p(o,a)}export{e as component};
--- Portfolio/svelte.config.js
+++ Portfolio/svelte.config.js
@@ -1,14 +1,23 @@
 import adapter from '@sveltejs/adapter-static';
 
-export default {
-  kit: {
-    adapter: adapter({
-      pages: 'docs',
-      assets: 'docs',
-      fallback: null
-    }),
-    paths: {
-      base: '', // oppure '/NOME-REPO' se usi custom domain o github.io
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+    kit: {
+        // adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
+        // If your environment is not supported, or you settled on a specific environment, switch out the adapter.
+        // See https://svelte.dev/docs/kit/adapters for more information about adapters.
+        adapter: adapter({
+            pages: 'docs',
+            assets: 'docs',
+            fallback: undefined,
+            precompress: false,
+            strict: true,
+        }),
+        paths: {
+            base: process.argv.includes('dev') ? '' : '/mysveltekit'
+        }
     }
-  }
 };
+
+export default config;
+
